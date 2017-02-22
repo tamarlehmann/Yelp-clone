13510a25b31e6c24a2e9c37c8b91e142c31d45e2
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-22 12:07:07 +0000

    V1 Chapter 14: Validations on restaurant uniqueness and review ratings over 5

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe Review, type: :model do
+    it "is invalid if the rating is more than 5" do
+      review = Review.new(rating: 10)
+      expect(review).to have(1).error_on(:rating)
+    end
+  end
 end

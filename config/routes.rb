31b3754ad632f5982c66bc632d05679edb145e8e
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-23 11:28:30 +0000

    V2 Challenge 4: Added facebook log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  root "restaunts#index"
-  devise_for :users
+  root "restaurants#index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'restaurants' => 'restaurants#index'
 

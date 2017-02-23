9b55a42fc8ac592b3958a7bda8c2b726147ef0a3
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-23 18:08:46 +0000

    Passed test for Restaurant naming problems

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe Restaurant, type: :model do
   end
 
   it "is not valid unless it has a unique name" do
-    Restaurant.create(name: "Moe's Tavern")
+    @user = User.create(email: 'test@test.com', password: 'test123')
+    @user.restaurants.create(name: "Moe's Tavern")
     restaurant = Restaurant.new(name: "Moe's Tavern")
     expect(restaurant).to have(1).error_on(:name)
   end
